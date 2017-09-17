acb5577548af04e31bb666f4bfcea71f6d232290
Aristote Diasonama <aristotediasonama@yahoo.fr>
2017-09-17 18:45:19 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import {Helmet} from "react-helmet";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Helmet>
+          <title>Ship your stuff with Diaspoship!</title>
+        </Helmet>
+      </div>
+    );
+  }
+}
+
+export default App;
