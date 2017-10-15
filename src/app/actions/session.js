cb3a7596e933dc5c761146ffb4fbb08d9a95ffb2
Diouf, Patrice <pdiouf@cebglobal.com>
2017-10-15 19:43:47 -0400

    forget .env

--- src/app/actions/session.js
+++ src/app/actions/session.js
@@ -1,9 +1,9 @@
 import { createAction } from 'redux-actions';
+import { FB_APP_ID } from '../../config/environment';
 export const resetUser = createAction('LOGOUT_USER');
 export const isUserConnected = createAction('IS_USER_CONNECTED');
 export const userIsConnecting = createAction('USER_IS_CONNECTING');
 export const setUser = createAction('USER_FETCH_DATA_SUCCESS');
-const FB_APP_ID = process.env.REACT_APP_SECRET_FB_APP_ID;
 
 export function login() {
   return dispatch => {
